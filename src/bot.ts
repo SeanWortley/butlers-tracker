1eb7017250211443df11460cf5b8bb99e420273d
pank <202449477+SeanWortley@users.noreply.github.com>
2026-07-02 11:03:13 +0200

    Added cash order processing

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,16 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard, Context } from "grammy";
+import { conversations, createConversation } from "@grammyjs/conversations";
+import type { Conversation, ConversationFlavor } from "@grammyjs/conversations";
 import { recordText } from "./app.ts";
 import { shiftFor } from "./store.ts";
+import { type Order, earnings } from "./order.ts";
+
+type MyContext = ConversationFlavor<Context>;
 
 export function initializeBot() {
-    const bot = new Bot(process.env.BOT_TOKEN!);
-    bot.start();
+    const bot = new Bot<MyContext>(process.env.BOT_TOKEN!);
+    bot.use(conversations());
+    bot.use(createConversation(cashOrder))
 
     bot.on(["message:text", "message:caption"], (ctx) => {
         const userID = ctx.from?.id;
@@ -15,6 +21,41 @@ export function initializeBot() {
 
         console.log(text);
         const reply = recordText(shift, text);
-        if (reply) ctx.reply(reply);
+        if (reply) sendTotal(ctx, reply);
     })
+
+    bot.callbackQuery("enter_cash", async (ctx) => {
+        await ctx.answerCallbackQuery();
+        await ctx.conversation.enter("cashOrder");
+    })
+
+    bot.start();
+}
+
+async function cashOrder(conversation: Conversation, ctx: Context) {
+    const userID = ctx.from?.id;
+    if (!userID) return;
+
+    await ctx.reply("Cash received?");
+    const received = await conversation.form.number(async (ctx) => {
+        await ctx.reply("Cash order cancelled.")
+    });
+
+    await ctx.reply("Order total?");
+    const total = await conversation.form.number(async (ctx) => {
+        await ctx.reply("Cash order cancelled.")
+    });
+
+    const order: Order = { type: "cash", received: received, total: total };
+
+    const shift = shiftFor(ctx.from?.id);
+    shift.addOrder(order);
+
+    sendTotal(ctx, `You made R${earnings(order).toFixed(2)}. Shift total: R${shift.getTotal().toFixed(2)}`)
+
+}
+function sendTotal(ctx: Context, text: string) {
+    const cashButton = new InlineKeyboard().text("Enter cash order", "enter_cash");
+    ctx.reply(text, { reply_markup: cashButton });
+
 }
